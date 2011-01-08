2198ce571fe494e60f94a5db7606fd80155dd5e0
svn <svn@0afe67c7-cec8-49f3-8395-211bb172ef3f>
2011-01-08 01:52:34 +0000

    asdf
    git-svn-id: file:///var/svn/seriesoftubes@17 0afe67c7-cec8-49f3-8395-211bb172ef3f

--- scripts/subtractBam.py
+++ scripts/subtractBam.py
@@ -101,8 +101,7 @@ class SubtractBamFilenameParser(scripter.FilenameParser):
         input_dir_parts = self.input_dir.split(os.sep)
         glob_path = ['mapped', input_dir_parts[0],'*'] + input_dir_parts[2:] + [
                           os.path.basename(self.input_file)]
-        potential_filenames = glob.glob(os.path.join(
-                                            ))
+        potential_filenames = glob.glob(os.path.join(glob_path))
 
         if len(potential_filenames) is 1:
             self.mapped_file = potential_filenames[0]
